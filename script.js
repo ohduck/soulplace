9248f346de95672e919f1f47de7755eedf7ab3be
prashant <prashantj724@gmail.com>
2019-01-17 16:31:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.parallax').parallax();
+  $('.carousel').carousel();{
+    fullWidth:true;
+  }
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+});
